98d8fd08abe0480d5cbb2d76d3de2e24528754a9
Alex <ekaterinavaleeeva@yandex.ru>
2025-02-25 23:22:25 +0300

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { WebApp } from "@twa-dev/sdk";  // ✅ Добавили импорт
 import App from './App.tsx'
 import './index.css'
+
+WebApp.ready();  // ✅ Добавили инициализацию WebApp
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
